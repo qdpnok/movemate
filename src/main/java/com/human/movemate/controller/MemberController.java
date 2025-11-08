0836fe6da0e9574b9ec526720072e664e8e46740
LSM <qdpnok@naver.com>
2025-11-08 17:32:31 +0900

    work js

--- src/main/java/com/human/movemate/controller/MemberController.java
+++ src/main/java/com/human/movemate/controller/MemberController.java
@@ -1,10 +1,13 @@
 package com.human.movemate.controller;
 
+import com.human.movemate.model.Member;
 import com.human.movemate.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,6 +19,14 @@ public class MemberController {
 
     // Get방식으로 루트경로/member/new 로 요청이 들어오면 members/signup 페이지를 렌더링
     @GetMapping("/new")
-    public String signup() { return "members/new"; }
+    public String signupPage(Model model) {
+        model.addAttribute("memberFrom", new Member());
+        return "members/new"; }
+
+    @PostMapping("/new")
+    public String signup(Member member, Model model) {
+        memberService.signup(member);
+        return "redirect:/";
+    }
 
 }
