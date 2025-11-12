ebb394b2c370efefb126f99ee008b7cdb9eb17ec
HJ <pojki1004@naver.com>
2025-11-12 17:43:35 +0900

    회원가입 테스트 변경

--- src/main/java/com/human/movemate/dao/MemberDao.java
+++ src/main/java/com/human/movemate/dao/MemberDao.java
@@ -33,7 +33,7 @@ public class MemberDao {
         // VALUES ( ?, ?, ..., ? ) <- 위에서 작성한 [삽입할 칼럼 개수]와 [? 개수]가 맞아야 함.
         @Language("SQL")
         String sql = """
-        INSERT INTO member (name, user_id, password, email, phone_no)
+        INSERT INTO USERS (name, user_id, password, email, phone_no)
         VALUES (?, ?, ?, ?, ?)
         """;
 
@@ -59,7 +59,7 @@ public class MemberDao {
         // FROM [테이블명]
         // WHERE [조건] -> 특별한 조건이 없다면 생략, ?로 값 대입 가능
         @Language("SQL")
-        String sql = "SELECT * FROM member WHERE user_id = ?";
+        String sql = "SELECT * FROM USERS WHERE user_id = ?";
 
         // jdbc.query : 여러 행의 값을 가져올 때 사용함
         // query( sql, new RowMapper(), ?에 답을 값1, ... , ?에 담을 값 );
@@ -86,7 +86,7 @@ public class MemberDao {
     public boolean update(Long no, Member member){
         // UPDATE [테이블명] SET [변경할 칼럼명1] = ?, ... , [변경할 칼럼명n] = ? WHERE [조건]
         @Language("SQL")
-        String sql = "UPDATE member SET pwd = ?, name =? WHERE user_no = ?";
+        String sql = "UPDATE USERS SET pwd = ?, name =? WHERE user_no = ?";
 
         // jdbc.update : 데이터 삽입, 수정, 삭제에 사용함.
         // update( sql, ?에 담을 값 1, ... , ?에 담을 값 n);
@@ -108,7 +108,7 @@ public class MemberDao {
     public boolean delete(Long no){
         // DELETE [테이블명] WHERE [조건]
         @Language("SQL")
-        String sql = "DELETE member WHERE user_no = ?";
+        String sql = "DELETE USERS WHERE user_no = ?";
 
         // jdbc.update : 데이터 삽입, 수정, 삭제에 사용함.
         // update( sql, ?에 담을 값 1, ... , ?에 담을 값 n);
